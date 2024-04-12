002d810eac705b8b395ff7a2ed3fe37a7dfda19c
bsekisser <squirmyworms@embarqmail.com>
2024-04-12 14:11:17 -0400

    quiet some compiler warnings.
    	modified:   include/armvm_core_shiftbox.h

--- libarmvm/include/armvm_core_shiftbox.h
+++ libarmvm/include/armvm_core_shiftbox.h
@@ -9,36 +9,47 @@
 
 /* **** */
 
+#include "libbse/include/unused.h"
+
+/* **** */
+
+UNUSED_FN
 static uint32_t _shifter_operand_immediate(armvm_core_p core)
 {
 	return(arm_shiftbox(ARM_SOP_ROR, ARM_IR_DPI_IMMEDIATE, ARM_IR_DPI_ROTATE_AMOUNT, IF_CPSR(C)));
 }
 
+UNUSED_FN
 static uint32_t _shifter_operand_c_immediate(armvm_core_p core)
 {
 	return(arm_shiftbox_c_immediate(ARM_SOP_ROR, ARM_IR_DPI_IMMEDIATE, ARM_IR_DPI_ROTATE_AMOUNT));
 }
 
+UNUSED_FN
 static uint32_t _shifter_operand_shift_immediate(armvm_core_p core)
 {
 	return(arm_shiftbox_immediate(ARM_IR_DP_SHIFT_TYPE, irGPR(M), ARM_IR_DP_SHIFT_AMOUNT, IF_CPSR(C)));
 }
 
+UNUSED_FN
 static uint32_t _shifter_operand_c_shift_immediate(armvm_core_p core)
 {
 	return(arm_shiftbox_c_immediate(ARM_IR_DP_SHIFT_TYPE, irGPR(M), ARM_IR_DP_SHIFT_AMOUNT));
 }
 
+UNUSED_FN
 static uint32_t _shifter_operand_shift_register(armvm_core_p core)
 {
 	return(arm_shiftbox_immediate(ARM_IR_DP_SHIFT_TYPE, irGPR(M), irGPR(S), IF_CPSR(C)));
 }
 
+UNUSED_FN
 static uint32_t _shifter_operand_c_shift_register(armvm_core_p core)
 {
 	return(arm_shiftbox_c_immediate(ARM_IR_DP_SHIFT_TYPE, irGPR(M), irGPR(S)));
 }
 
+UNUSED_FN
 static uint32_t _shifter_operand(armvm_core_p core)
 {
 	if(ARM_IR_DPI)
@@ -50,6 +61,7 @@ static uint32_t _shifter_operand(armvm_core_p core)
 	return(_shifter_operand_shift_immediate(core));
 }
 
+UNUSED_FN
 static uint32_t _shifter_operand_c(armvm_core_p core)
 {
 	if(ARM_IR_DPI)
