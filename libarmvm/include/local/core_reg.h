c1014d9bdd996431c8ad8488a4a81f973541fcb1
bsekisser <squirmyworms@embarqmail.com>
2024-04-24 22:21:07 -0400

    wip
    	modified:   core/makefile
    	modified:   include/armvm_core.h
    	modified:   include/local/core_reg.h
    	modified:   makefile
    	deleted:    trace/archive/armvm_trace_reg.c
    	deleted:    trace/armvm_trace.c
    	deleted:    trace/armvm_trace_arm.c
    	deleted:    trace/makefile

--- libarmvm/include/local/core_reg.h
+++ libarmvm/include/local/core_reg.h
@@ -14,12 +14,6 @@
 
 /* **** */
 
-//#define rRx(_x) core->armvm_trace.vr[_x]
-//#define rR(_x) rRx(ARMVM_TRACE_R(_x))
-
-//#define vRx(_x) core->armvm_trace.vr[_x]
-//#define vR(_x) vRx(ARMVM_TRACE_R(_x))
-
 #ifndef rR_IS_PC
 	#define rR_IS_PC(_x) (ARMVM_GPR(PC) == ARM_IR_R(_x))
 #endif
@@ -30,16 +24,32 @@
 
 /* **** */
 
+static void setup_rR(armvm_core_p const core, const unsigned rrx, const unsigned r)
+{ rRx(rrx) = r; vRx(rrx) = 0xfeedface; }
+
 static uint32_t setup_rR_vR(armvm_core_p const core, const unsigned rrx,
 	const unsigned r, const uint32_t v)
 {
-//	if(core->config.trace) {
-		rRx(rrx) = r;
-		vRx(rrx) = v;
-//	}
+	rRx(rrx) = r;
+	vRx(rrx) = v;
 
 	return(v);
 }
+
+UNUSED_FN
+static void setup_vR(armvm_core_p const core, const unsigned rrx, const uint32_t v)
+{ rRx(rrx) = ~0U; vRx(rrx) = v; }
+
+/* **** */
+
+static void core_reg_dst(armvm_core_p const core, const unsigned rrx,
+	const unsigned r)
+{ setup_rR(core, rrx, r & 15); }
+
+UNUSED_FN
+static void core_reg_dst_decode(armvm_core_p const core, const unsigned rrx,
+	const unsigned msb, const unsigned lsb)
+{ return(core_reg_dst(core, rrx, mlBFEXT(IR, msb, lsb))); }
 
 static uint32_t core_reg_dst_wb(armvm_core_p const core, const unsigned rrx,
 	const unsigned r, const uint32_t v)
@@ -51,11 +61,14 @@ static uint32_t core_reg_dst_wb(armvm_core_p const core, const unsigned rrx,
 	return(setup_rR_vR(core, rrx, rr, v));
 }
 
+UNUSED_FN
+static void core_reg_wb(armvm_core_p const core, const unsigned rrx)
+{ if(CCX) GPRx(rRx(rrx)) = vRx(rrx); }
+
 static uint32_t core_reg_src(armvm_core_p const core, const unsigned rrx,
 	const unsigned r)
 {
 	const unsigned rr = r & 15;
-//	const uint32_t v = GPRx(rr) + ((!!(ARMVM_GPR(PC) == r)) << 2);
 	uint32_t v = GPRx(rr);
 
 	if(ARMVM_GPR(PC) == r)
@@ -63,3 +76,8 @@ static uint32_t core_reg_src(armvm_core_p const core, const unsigned rrx,
 
 	return(setup_rR_vR(core, rrx, rr, v));
 }
+
+UNUSED_FN
+static uint32_t core_reg_src_decode(armvm_core_p const core, const unsigned rrx,
+	const unsigned msb, const unsigned lsb)
+{ return(core_reg_src(core, rrx, mlBFEXT(IR, msb, lsb))); }
