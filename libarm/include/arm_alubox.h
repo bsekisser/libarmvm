42383e38cce956dddd0d5465ee66b70f85939d79
bsekisser <squirmyworms@embarqmail.com>
2024-05-13 11:22:47 -0400

    wip

--- libarm/include/arm_alubox.h
+++ libarm/include/arm_alubox.h
@@ -19,14 +19,14 @@ typedef struct arm_alubox_t {
 	uint32_t cpsr;
 }alubox_t;
 
-int arm_alubox(arm_alubox_p alu, unsigned operation,
+uint32_t arm_alubox(arm_alubox_p alu, unsigned operation,
 	const arm_gpr_t rd, const arm_gpr_t rn,
 		const uint32_t sop_in);
 
-int arm_alubox_shift_immediate(arm_alubox_p const alu, const unsigned alu_operation,
-	const arm_gpr_t rd, const arm_gpt_t rn,
-	const unsigned shift_type, const arm_gpr_t rm, const uint8_t rs);
+uint32_t arm_alubox_shift_immediate(arm_alubox_p const alu,
+	const unsigned alu_operation, const unsigned shift_type,
+	const arm_gpr_t rm, const uint8_t rs);
 
-int arm_alubox_shift_register(arm_alubox_p const alu, const unsigned alu_operation,
-	const arm_gpr_t rd, const arm_gpr_t rn,
-	const unsigned shift_type, const arm_gpr_t rm, const arm_gpr_t rs);
+uint32_t arm_alubox_shift_register(arm_alubox_p const alu,
+	const unsigned alu_operation, const unsigned shift_type,
+	const arm_gpr_t rm, const arm_gpr_t rs);
--- libarm/source/arm_alubox.c
+++ libarm/source/arm_alubox.c
@@ -142,10 +142,9 @@ uint32_t arm_alubox(arm_alubox_p alu, const unsigned alu_operation,
 }
 
 uint32_t arm_alubox_shift_immediate(arm_alubox_p const alu, const unsigned alu_operation,
-	const arm_gpr_t rd, const arm_gpt_t rn,
 	const unsigned shift_type, const arm_gpr_t rrm, const uint8_t rs)
 {
-	uint32_t sop = 0;
+	const uint32_t sop = arm_shiftbox(shift_type, arm->gpr[rrm]. rs);
 
 	if(rs)
 		sop = _arm_alubox_shift(alu, shift_type, rrm, rs);
