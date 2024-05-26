e72832bf4e33510966cbf7d91cda846441821e49
bsekisser <squirmyworms@embarqmail.com>
2024-05-26 12:27:59 -0400

    test_arm_rsb
    	modified:   source/test_arm_rsb.cpp

--- libarmvm/test/source/test_arm_rsb.cpp
+++ libarmvm/test/source/test_arm_rsb.cpp
@@ -1,24 +1,22 @@
-extern "C" {
-	#include "test_utility.h"
-	#include "test.h"
-}
+#include "test_arm.hpp"
+#include "test_utility.hpp"
+#include "test.hpp"
 
 /* **** */
 
-#include "test_arm_gen.hpp"
+#include "libarmcc/include/armcc.hpp"
 
 /* **** */
 
-extern "C" {
-	int test_arm_rsbs(test_p t)
+int test_arm::rsbs(void)
 {
-	reset(t);
+	reset();
 
 	rR(0) = 1;
-	rsbs(r0, r0, LSL(r0, 3));
-	run_test(t);
+	cc.rsbs(r0, r0, cc.lsl(r0, 3));
+	run_test();
 	fail_if(0x00000007 != rR(0));
-	check_nzcv(t, 0, 0, 1, 0);
+	check_nzcv(0, 0, 1, 0);
 
 	return(0);
-}}
+}
