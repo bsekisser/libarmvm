cdbc2d7561205f12dd07f92f6494667a1e9476b6
bsekisser <squirmyworms@embarqmail.com>
2024-05-25 22:43:58 -0400

    test_arm_shift
    	modified:   source/include/test_arm.hpp
    	modified:   source/test_arm_shift.cpp

--- libarmvm/test/source/test_arm_shift.cpp
+++ libarmvm/test/source/test_arm_shift.cpp
@@ -1,92 +1,90 @@
-extern "C" {
-	#include "test_utility.h"
-	#include "test.h"
-}
+#include "test_arm.hpp"
+#include "test_utility.hpp"
+#include "test.hpp"
 
 /* **** */
 
-#include "test_arm_gen.hpp"
+#include "libarmcc/include/armcc.hpp"
 
 /* **** */
 
-extern "C" {
-	int test_arm_shift(test_p t)
+int test_arm::shift()
 {
-	reset(t);
+	reset();
 
 	rR(1) = 1;
-	asrs(r0, r1, 0);
-	run_test(t);
+	cc.asrs(r0, r1, 0);
+	run_test();
 	fail_if(0 != rR(0));
-	check_nzc(t, 0, 1, 0);
+	check_nzc(0, 1, 0);
 
 	rR(1) = 0x40000000;
-	asrs(r0, r1, 0);
-	run_test(t);
+	cc.asrs(r0, r1, 0);
+	run_test();
 	fail_if(0 != rR(0));
-	check_nzc(t, 0, 1, 0);
+	check_nzc(0, 1, 0);
 
 	rR(1) = 0x80000000;
-	asrs(r0, r1, 31);
-	run_test(t);
+	cc.asrs(r0, r1, 31);
+	run_test();
 	fail_if(-1U != rR(0));
-	check_nzc(t, 1, 0, 0);
+	check_nzc(1, 0, 0);
 
 	rR(1) = 0x80000000;
-	asrs(r0, r1, 0);
-	run_test(t);
+	cc.asrs(r0, r1, 0);
+	run_test();
 	fail_if(-1U != rR(0));
-	check_nzc(t, 1, 0, 1);
+	check_nzc(1, 0, 1);
 
-	movs(r1, ROR(0x80, 8));
-	asrs(r1, r1, 0);
-	lsls(r1, r1, 16);
-	lsrs(r1, r1, 8);
-	run_test(t);
+	cc.movs(r1, cc.ror(0x80, 8));
+	cc.asrs(r1, r1, 0);
+	cc.lsls(r1, r1, 16);
+	cc.lsrs(r1, r1, 8);
+	run_test();
 	fail_if(0x00ffff00 != rR(1));
-	check_nzc(t, 0, 0, 0);
+	check_nzc(0, 0, 0);
 
-	movs(r1, ROR(1, 16));
-	run_test(t);
+	cc.movs(r1, cc.ror(1, 16));
+	run_test();
 	fail_if(0x00010000 != rR(1));
-	check_nzc(t, 0, 0, 0);
+	check_nzc(0, 0, 0);
 
-	movs(r1, ROR(1, 30));
-	run_test(t);
+	cc.movs(r1, cc.ror(1, 30));
+	run_test();
 	fail_if(0x00000004 != rR(1));
-	check_nzc(t, 0, 0, 0);
+	check_nzc(0, 0, 0);
 
 	rR(1) = 0x00000005;
-	lsls(r0, r1, 29);
-	run_test(t);
+	cc.lsls(r0, r1, 29);
+	run_test();
 	fail_if(0xa0000000 != rR(0));
-	check_nzc(t, 1, 0, 0);
+	check_nzc(1, 0, 0);
 
-		lsls(r0, r1, 30);
-		step_test(t);
+		cc.lsls(r0, r1, 30);
+		step_test();
 		fail_if(0x40000000 != rR(0));
-		check_nzc(t, 0, 0, 1);
+		check_nzc(0, 0, 1);
 
-		lsls(r0, r1, 31);
-		step_test(t);
+		cc.lsls(r0, r1, 31);
+		step_test();
 		fail_if(0x80000000 != rR(0));
-		check_nzc(t, 1, 0, 0);
+		check_nzc(1, 0, 0);
 
 	rR(1) = 0xffffffe6;
-	lsls(r0, r1, 30);
-	run_test(t);
+	cc.lsls(r0, r1, 30);
+	run_test();
 	fail_if(0x80000000 != rR(0));
-	check_nzc(t, 1, 0, 1);
+	check_nzc(1, 0, 1);
 
-		lsls(r0, r1, 31);
-		step_test(t);
+		cc.lsls(r0, r1, 31);
+		step_test();
 		fail_if(0x00000000 != rR(0));
-		check_nzc(t, 0, 1, 1);
+		check_nzc(0, 1, 1);
 
-		lsls(r1, r1, 31);
-		step_test(t);
+		cc.lsls(r1, r1, 31);
+		step_test();
 		fail_if(0x00000000 != rR(1));
-		check_nzc(t, 0, 1, 1);
+		check_nzc(0, 1, 1);
 
 	return(1);
-}}
+}
