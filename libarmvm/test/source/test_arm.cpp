9d164cd2b46bb05274515f2821fe1234774b1ea0
bsekisser <squirmyworms@embarqmail.com>
2024-05-26 10:15:34 -0400

    test_arm_sub
    	modified:   test_arm.cpp
    	modified:   test_arm_sub.cpp

--- libarmvm/test/source/test_arm.cpp
+++ libarmvm/test/source/test_arm.cpp
@@ -1,4 +1,4 @@
-#include "test_arm.h"
+#include "test_arm.hpp"
 
 /* **** */
 
--- libarmvm/test/source/test_arm_sub.cpp
+++ libarmvm/test/source/test_arm_sub.cpp
@@ -1,78 +1,76 @@
-extern "C" {
-	#include "test_utility.h"
-	#include "test.h"
-}
+#include "test_arm.hpp"
+#include "test_utility.hpp"
+#include "test.hpp"
 
 /* **** */
 
-#include "test_arm_gen.hpp"
+#include "libarmcc/include/armcc.hpp"
 
 /* **** */
 
-extern "C" {
-	int test_arm_subs(test_p t)
+int test_arm::subs(void)
 {
-	reset(t);
+	reset();
 
 	rR(1) = 13; rR(2) = 12;
-	subs(r0, r1, r2);
-	run_test(t);
+	cc.subs(r0, r1, r2);
+	run_test();
 	fail_if(1 != rR(0));
-	check_nzcv(t, 0, 0, 1, 0);
+	check_nzcv(0, 0, 1, 0);
 
 	rR(1) = 12; rR(2) = 13;
-	subs(r0, r1, r2);
-	run_test(t);
+	cc.subs(r0, r1, r2);
+	run_test();
 	fail_if(-1U != rR(0));
-	check_nzcv(t, 1, 0, 0, 0);
+	check_nzcv(1, 0, 0, 0);
 
 	rR(1) = 0x1c; rR(2) = 0x1c;
-	subs(r0, r1, r2);
-	run_test(t);
+	cc.subs(r0, r1, r2);
+	run_test();
 	fail_if(0 != rR(0));
-	check_nzcv(t, 0, 1, 1, 0);
+	check_nzcv(0, 1, 1, 0);
 
 	rR(1) = 0x1d; rR(2) = 0x1c;
-	subs(r0, r1, r2);
-	run_test(t);
+	cc.subs(r0, r1, r2);
+	run_test();
 	fail_if(1 != rR(0));
-	check_nzcv(t, 0, 0, 1, 0);
+	check_nzcv(0, 0, 1, 0);
 
 	rR(0) = 0x1f; rR(1) = 0x09;
-	subs(r1, r1, LSR(r0, 4));
-	run_test(t);
+	cc.subs(r1, r1, cc.lsr(r0, 4));
+	run_test();
 	fail_if(0x00000008 != rR(1));
-	check_nzcv(t, 0, 0, 1, 0);
+	check_nzcv(0, 0, 1, 0);
 
 	rR(1) = 64 << 24; rR(2) = 65 << 24;
-	subs(r0, r1, r2);
-	run_test(t);
+	cc.subs(r0, r1, r2);
+	run_test();
 	fail_if((0xffU << 24) != rR(0));
-//	check_nzcv(t, 1, 0, 1, 1); // !
-	check_nzcv(t, 1, 0, 0, 0); // ?
+//	check_nzcv(1, 0, 1, 1); // !
+	check_nzcv(1, 0, 0, 0); // ?
 
 	rR(0) = 0; rR(1) = 0; rR(2) = 1;
-	subs(r1, r1, r2);
-	run_test(t);
+	cc.subs(r1, r1, r2);
+	run_test();
 	fail_if(~0U != rR(1));
-	check_nzcv(t, 1, 0, 0, 0);
+	check_nzcv(1, 0, 0, 0);
 
-		sbcs(r0, r0, r0);
-		step_test(t);
+		cc.sbcs(r0, r0, r0);
+		step_test();
 		fail_if(~0U != rR(0));
-		check_nzcv(t, 1, 0, 0, 0);
+		check_nzcv(1, 0, 0, 0);
 
 	rR(1) = 0x60000000; rR(2) = 0x20000000;
-	subs(r0, r1, r2);
-	run_test_flags(t, _BV(ARM_CPSR(C)));
+	cc.subs(r0, r1, r2);
+	run_test_flags(_BV(ARM_CPSR(C)));
 	fail_if(0x40000000 != rR(0));
-	check_nzcv(t, 0, 0, 1, 0);
+	check_nzcv(0, 0, 1, 0);
 
 	rR(1) = 0x60000000; rR(2) = 0xc0000000;
-	subs(r0, r1, r2);
-	run_test_flags(t, _BV(ARM_CPSR(C)));
+	cc.subs(r0, r1, r2);
+	run_test_flags(_BV(ARM_CPSR(C)));
 	fail_if(0xa0000000 != rR(0));
-	check_nzcv(t, 1, 0, 0, 1);
+	check_nzcv(1, 0, 0, 1);
 
-	return(1);
-}}
+	return(0);
+}
