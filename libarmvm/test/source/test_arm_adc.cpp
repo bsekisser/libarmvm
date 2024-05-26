4096d2a3a064e954ffed447a0d1e765aa9b311f1
bsekisser <squirmyworms@embarqmail.com>
2024-05-26 12:07:40 -0400

    test_arm_adc
    	modified:   source/test_arm_adc.cpp

--- libarmvm/test/source/test_arm_adc.cpp
+++ libarmvm/test/source/test_arm_adc.cpp
@@ -1,57 +1,55 @@
-extern "C" {
-	#include "test_utility.h"
-	#include "test.h"
-}
+#include "test_arm.hpp"
+#include "test_utility.hpp"
+#include "test.hpp"
 
 /* **** */
 
-#include "test_arm_gen.hpp"
+#include "libarmcc/include/armcc.hpp"
 
 /* **** */
 
-extern "C" {
-	int test_arm_adcs(test_p t)
+int test_arm::adcs(void)
 {
-	reset(t);
+	reset();
 
 	rR(1) = ~0; rR(2) = 1;
-	adcs(r0, r1, r2);
-	run_test(t);
+	cc.adcs(r0, r1, r2);
+	run_test();
 	fail_if(0 != rR(0));
-	check_nzcv(t, 0, 1, 1, 0);
+	check_nzcv(0, 1, 1, 0);
 
 	rR(1) = 12; rR(2) = 1;
-	adcs(r0, r1, r2);
-	run_test(t);
+	cc.adcs(r0, r1, r2);
+	run_test();
 	fail_if(13 != rR(0));
-	check_nzcv(t, 0, 0, 0, 0);
+	check_nzcv(0, 0, 0, 0);
 
 	rR(1) = -0; rR(2) = -1;
-	adcs(r0, r1, r2);
-	run_test(t);
+	cc.adcs(r0, r1, r2);
+	run_test();
 	fail_if(-1U != rR(0));
-	check_nzcv(t, 1, 0, 0, 0);
+	check_nzcv(1, 0, 0, 0);
 
 	rR(1) = -1; rR(2) = -1;
-	adcs(r0, r1, r2);
-	run_test(t);
+	cc.adcs(r0, r1, r2);
+	run_test();
 	fail_if(-2U != rR(0));
-	check_nzcv(t, 1, 0, 1, 0);
+	check_nzcv(1, 0, 1, 0);
 
 	rR(1) = _BV(31); rR(2) = _BV(31);
-	adcs(r0, r1, r2);
-	run_test(t);
+	cc.adcs(r0, r1, r2);
+	run_test();
 	fail_if(0 != rR(0));
-	check_nzcv(t, 0, 1, 1, 1);
+	check_nzcv(0, 1, 1, 1);
 
 	rR(0) = 0; rR(2) = ~0; rR(3) = 1;
-	adds(r1, r2, r3);
-	adcs(r0, r0, r0);
-	run_test(t);
+	cc.adds(r1, r2, r3);
+	cc.adcs(r0, r0, r0);
+	run_test();
 	fail_if(0 != rR(1));
 	fail_if(1 != rR(0));
-	check_nzcv(t, 0, 0, 0, 0);
+	check_nzcv(0, 0, 0, 0);
 
 
 	return(1);
-}}
+}
