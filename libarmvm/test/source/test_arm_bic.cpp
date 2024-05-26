6f574923bad6a18521d472fbb4bdac1aefac8941
bsekisser <squirmyworms@embarqmail.com>
2024-05-26 12:15:04 -0400

    test_arm_bic
    	modified:   source/test_arm_bic.cpp

--- libarmvm/test/source/test_arm_bic.cpp
+++ libarmvm/test/source/test_arm_bic.cpp
@@ -1,42 +1,40 @@
-extern "C" {
-	#include "test_utility.h"
-	#include "test.h"
-}
+#include "test_arm.hpp"
+#include "test_utility.hpp"
+#include "test.hpp"
 
 /* **** */
 
-#include "test_arm_gen.hpp"
+#include "libarmcc/include/armcc.hpp"
 
 /* **** */
 
-extern "C" {
-	int test_arm_bics(test_p t)
+int test_arm::bics(void)
 {
-	reset(t);
+	reset();
 
 	rR(1) = 0xffffffff; rR(2) = 0x0f0f0f0f;
-	bics(r0, r1, r2);
-	run_test(t);
+	cc.bics(r0, r1, r2);
+	run_test();
 	fail_if(0xf0f0f0f0 != rRx(0));
-	check_nz(t, 1, 0);
+	check_nz(1, 0);
 
 	rR(1) = 0xffffffff; rR(2) = 0xf0f0f0f0;
-	bics(r0, r1, r2);
-	run_test(t);
+	cc.bics(r0, r1, r2);
+	run_test();
 	fail_if(0x0f0f0f0f != rRx(0));
-	check_nz(t, 0, 0);
+	check_nz(0, 0);
 
 	rR(1) = 0x0f0f0f0f; rR(2) = 0xf0f0f0f0;
-	bics(r0, r1, r2);
-	run_test(t);
+	cc.bics(r0, r1, r2);
+	run_test();
 	fail_if(0x0f0f0f0f != rRx(0));
-	check_nz(t, 0, 0);
+	check_nz(0, 0);
 
 	rR(1) = 0x0f0f0f0f; rR(2) = 0x0f0f0f0f;
-	bics(r0, r1, r2);
-	run_test(t);
+	cc.bics(r0, r1, r2);
+	run_test();
 	fail_if(0 != rRx(0));
-	check_nz(t, 0, 1);
+	check_nz(0, 1);
 
 	return(1);
-}}
+}
